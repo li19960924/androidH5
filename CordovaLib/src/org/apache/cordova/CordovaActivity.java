93cc641177420fa6704a3d09824f4e3f085d92f4
lijie <1763844812@qq.com>
2017-07-19 20:55:55 +0800

    7/19

--- CordovaLib/src/org/apache/cordova/CordovaActivity.java
+++ CordovaLib/src/org/apache/cordova/CordovaActivity.java
@@ -295,20 +295,20 @@ public class CordovaActivity extends MyActivity {
     /**
      * Called when view focus is changed
      */
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus && immersiveMode) {
-            final int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-
-            getWindow().getDecorView().setSystemUiVisibility(uiOptions);
-        }
-    }
+//    @Override
+//    public void onWindowFocusChanged(boolean hasFocus) {
+//        super.onWindowFocusChanged(hasFocus);
+//        if (hasFocus && immersiveMode) {
+//            final int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+//
+//            getWindow().getDecorView().setSystemUiVisibility(uiOptions);
+//        }
+//    }
 
     @SuppressLint("NewApi")
     @Override
